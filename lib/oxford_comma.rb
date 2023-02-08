d23aea54f72b45b8616095f27e43a5ed4b3feb38
Jamila <jamnjeri@gmail.com>
2023-02-08 10:55:36 +0300

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        # Add an and
+        array[-1] = "and #{array[-1]}"
+        return array.join (" ")
+    elsif array.size >= 3 
+        # Add an and
+        array[-1] = "and #{array[-1]}"
+        # Add a space seperator
+        return array.join (", ")
+    end 
 end
